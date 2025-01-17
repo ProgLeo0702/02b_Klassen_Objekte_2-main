73c07e055885da4eed870f217b1f3b69421dc369
ProgLeo0702 <leon-ahner@gmx.de>
2025-01-17 10:50:36 +0100

    01 / Wertzuweisung /Wertausweisung /Typkonvertierung (explizit)

---
+++ com/cc/java/App.java
@@ -0,0 +1,46 @@
+package com.cc.java;
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        // output("Blick von aussen: " + cat);
+        // output("Blick von innen: " + cat.getInstanceVariable());
+
+
+        // Werte zuweisen
+            cat.firstName = "Grizabella";
+            cat.furColor ="white";
+            cat.age = 29;
+
+        // Werte ausgeben
+             output(cat.firstName);
+             output(cat.furColor);
+             output(Integer.toString(cat.age));  // Explizite Typkonvertierung
+
+        output("-----------------------");
+
+        Cat cat2 = new Cat();
+        // output("Blick von aussen: " + cat2);
+        // output("Blick von innen: " + cat2.getInstanceVariable());
+
+        // Werte zuweisen
+        cat2.firstName = "Alonzo";
+        cat2.furColor = "grey";
+        cat2.age = 35;
+
+        // Werte ausgeben
+        output(cat2.firstName);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age));
+    }
+
+  
+    public static void output(String outputStr){
+        System.out.println(outputStr);
+    }
+
+
+}
+
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,21 @@
+package com.cc.java;
+
+public class Cat {
+
+    public String firstName;
+    public String furColor;
+    int age;
+
+
+
+
+
+
+
+    public Cat getInstanceVariable(){
+       return this;
+    }
+    
+
+
+}
